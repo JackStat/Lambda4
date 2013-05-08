ed4c56c63635e2bc07e2171004937b3fcf6cd0c0
JackStat <tyler.hunt@utah.edu>
2013-05-08 15:24:41 -0600

    fixed test

--- inst/tests/test-cov.lambda4.R
+++ inst/tests/test-cov.lambda4.R
@@ -1,14 +1,14 @@
-context("Angoff")
-test_that('angoff and population covariance matrices',{
+context("Covariance Lambda4")
+test_that('Covariance Lambda4 and population covariance matrices',{
   
-  test.val1<-as.numeric(round(cov.lambda4(oneFcong)[[1]],6))
-  expect_that(test.val1, equals(.855697))
+  test.val1<-as.numeric(round(cov.lambda4(oneFcong)[[1]][[1]],6))
+  expect_that(test.val1, equals(.854614))
   
-  test.val2<-as.numeric(round(cov.lambda4(oneFpar)[[1]],6))
+  test.val2<-as.numeric(round(cov.lambda4(oneFpar)[[1]][[1]],6))
   expect_that(test.val2, equals(.888889))
   
-  test.val3<-as.numeric(round(cov.lambda4(oneFtau)[[1]],6))
-  expect_that(test.val3, equals(.833968))
+  test.val3<-as.numeric(round(cov.lambda4(oneFtau)[[1]][[1]],6))
+  expect_that(test.val3, equals(.833575))
   
   
 })
