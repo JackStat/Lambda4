0e8d90dcb713e9799c632dde0c2d9af0cc8f9f93
JackStat <tyler.hunt@utah.edu>
2013-05-08 14:59:37 -0600

    added a test changed objects

--- inst/tests/test-angoff.R
+++ inst/tests/test-angoff.R
@@ -1,11 +1,14 @@
 context("Angoff")
 test_that('angoff and population covariance matrices',{
   
-  angoff.val1<-as.numeric(round(angoff(oneFcong)[[1]],6))
-  expect_that(angoff.val1, equals(.855697))
+  test.val1<-as.numeric(round(angoff(oneFcong)[[1]],6))
+  expect_that(test.val1, equals(.855697))
   
-  angoff.val2<-as.numeric(round(angoff(oneFpar)[[1]],6))
-  expect_that(angoff.val2, equals(.888889))
+  test.val2<-as.numeric(round(angoff(oneFpar)[[1]],6))
+  expect_that(test.val2, equals(.888889))
+  
+  test.val3<-as.numeric(round(angoff(oneFtau)[[1]],6))
+  expect_that(test.val3, equals(.833968))
   
   
 })
