7a39d2d5a5fd8b91a4186ecb0a8c5f0c080e20c6
JackStat <tyler.hunt@utah.edu>
2013-05-13 16:24:31 -0600

    2nd fix for the multiple splits

--- R/cov.lambda4.R
+++ R/cov.lambda4.R
@@ -90,9 +90,18 @@ cov.lambda4<-function (x, method="Hunt", missing = "complete", show.lambda4s = F
       
       sl4 <- sort(l4.vect)
       Min.Split <- t1t.matrix[which(l4.vect == sl4[1]), ]
-      Median.Split <- t1t.matrix[which(l4.vect == sl4[round(lencombs/2)]), ][1,]
-      Max.Split <- t1t.matrix[which(l4.vect == sl4[lencombs]), ][1,]
-      Splits <- data.frame(Min.Split, Median.Split, Max.Split)[,1]
+      
+      Median.Split <- t1t.matrix[which(l4.vect == sl4[round(lencombs/2)]), ]
+      if(nrow(Median.Split)>1)
+        {Median.Split=Median.Split[1,]}
+        {Median.Split=Median.Split}
+      
+      Max.Split <- t1t.matrix[which(l4.vect == sl4[lencombs]), ]
+      if(nrow(Max.Split)>1)
+        {Max.Split=Max.Split[1,]}
+        {Max.Split=Max.Split}
+      
+      Splits <- data.frame(Min.Split, Median.Split, Max.Split)
       
       count <- lencombs
       lambda4 <- data.frame(Mean, Max, Median, Minimum)
