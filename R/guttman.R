bbd1180590afd59bae97851993bfb0469101ded3
JackStat <tyler.hunt@utah.edu>
2013-06-24 09:19:03 -0600

    added returned parameters to documentation.

--- R/guttman.R
+++ R/guttman.R
@@ -2,6 +2,14 @@
 #' 
 #' @description Calculates all 6 of Guttman's lambda coefficients.
 #' 
+#' @return
+#' \item{Lambda1}{Guttman's Lambda1 estimate of reliability.}
+#' \item{Lambda2}{Guttman's Lambda2 estimate of reliability.}
+#' \item{Lambda3}{Guttman's Lambda3 estimate of reliability. Also known as Cronbach's alpha or coefficient alpha.}
+#' \item{Lambda4}{Guttman's maximimal Lambda4 estimate of reliability.}
+#' \item{Lambda5}{Guttman's Lambda5 estimate of reliability.}
+#' \item{Lambda6}{Guttman's Lambda6 estimate of reliability.}
+#' 
 #' @param x Can be either a data matrix or a covariance matrix
 #' @param missing How to handle missing values.
 #' @param standardize When TRUE Results are standardized by using the correlation matrix instead of the covariance matrix for computation.
@@ -32,7 +40,12 @@ guttman <- function(x, missing="complete", standardize=FALSE){
   Lambda6 <- as.numeric(lambda6(sigma))
 
 
-  result <- list(Lambda1=Lambda1, Lambda2=Lambda2, Lambda3=Lambda3, Lambda4=Lambda4, Lambda5=Lambda5, Lambda6=Lambda6)
+  result <- list(Lambda1=Lambda1, 
+                 Lambda2=Lambda2, 
+                 Lambda3=Lambda3, 
+                 Lambda4=Lambda4, 
+                 Lambda5=Lambda5, 
+                 Lambda6=Lambda6)
   
   class(result) <- c("guttman")
   return(result)
